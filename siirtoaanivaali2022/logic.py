566f629e799591a58319703532ea7feb3591873a
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2023-11-20 04:17:19 +0200

    Create repository

---
+++ siirtoaanivaali2022/logic.py
@@ -0,0 +1,22 @@
+import opavote
+
+print("Hello world")
+
+# TODO: luo lista ehdokkaista
+# TODO: luo lista lipukkeista
+
+# lue tiedosto ja luo lipukkeet. anna ehdokas- ja lipukelistat opavoten käyttöön
+
+# laskentakierros, toista kunnes valittuja tarpeeksi
+
+#   1. Ehdokas:Ehdokkaat laske äänet
+
+#   2. Lipukkeet äänihukka
+
+#   3. Äänikynnys update
+
+#   4. Ehdokas:Ehdokkaat ehdokas.updateP
+
+#   Ehdokkaat valitse äänikynnyksen ylittäneet tai pudota vähiten ääniä saanut
+#       jos tasapeli, toista laskentakierrosta poikkeavilla säännöillä (tee laskentakierroksesta oma funktio ja kutsu sitä rekursiivisesti)
+
---
+++ siirtoaanivaali2022/opavote.py
@@ -0,0 +1,37 @@
+def opavoteTest():
+    return "OpaVote says hello!"
+
+def luo_lipukkeet(tiedosto: str):
+    with open(tiedosto) as tiedosto:
+        tila = "ekarivi"
+        laskuri = 1
+        # TODO: luo lista ehdokkaista
+        # TODO: luo lista lipukkeista
+        for rivi in tiedosto:
+            rivi = rivi.strip()
+            if tila == "ekarivi":
+                osat = rivi.split(" ")
+                ehdokas_lkm = int(osat[0])
+                # TODO: lisää ehdokkaat listaan
+                valittavat_lkm = int(osat[1])
+                tila = "lipukkeet"
+            elif tila == "lipukkeet":
+                osat = rivi.split(" ")
+                if osat[0] == '0':
+                    tila = "ehdokkaat"
+                else:
+                    print(osat[1:-1])
+                    # TODO: luo lipuke listaan
+                    # TODO: luo lipukerivit lipukkeelle
+                    # lipuketta luodessa anna paremetrina ehdokkaat järjestyksessä
+            elif tila == "ehdokkaat":
+                if laskuri <= ehdokas_lkm:
+                    print(f"{laskuri} {rivi}")
+                    # TODO: lisää ehdokkaalle nimi
+                    laskuri = laskuri + 1
+                else:
+                    print("Vaalin nimi: " + rivi.strip('"'))
+                    # TODO: tallenna vaalin nimi sopivaan paikkaan
+
+if __name__ == "__main__":
+    luo_lipukkeet("testivaali.txt")
