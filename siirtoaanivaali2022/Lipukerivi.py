11e7dcf5043405e3ca8bc7f9d8179667f73ff26d
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2023-11-20 05:14:48 +0200

    Renaming Ehdokas and Lipukerivi

--- siirtoaanivaali2022/Lipukerivi.py
+++ siirtoaanivaali2022/Lipukerivi.py
@@ -1,8 +1,8 @@
-import ehdokas
+import siirtoaanivaali2022.Ehdokas as Ehdokas
 
 class Lipukerivi:
 
-    def __init__(self, ehdokas: ehdokas, edellinen_ehdokas : ehdokas, aanimaara : float):
+    def __init__(self, ehdokas: Ehdokas, edellinen_ehdokas : Ehdokas, aanimaara : float):
         self.ehdokas = ehdokas
         self.edellinen_ehdokas = edellinen_ehdokas
         self.aanimaara = aanimaara
