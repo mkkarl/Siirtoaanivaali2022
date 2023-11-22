81f7dc832cdb6d521301bf737790e58c7f669352
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2023-11-22 12:27:54 +0200

    Add Lipukkeet lisaa_lipuke, __str__

--- siirtoaanivaali2022/lipukkeet.py
+++ siirtoaanivaali2022/lipukkeet.py
@@ -1,4 +1,29 @@
+from Lipuke import Lipuke
+from Ehdokas import Ehdokas
+
 class Lipukkeet:
 
     def __init__(self) -> None:
         self.lipukkeet = []
+
+    def lisaa_lipuke(self, lipuke : Lipuke):
+        self.lipukkeet.append(lipuke)
+
+    def __str__(self) -> str:
+        mjono = ""
+        n = 1
+
+        for lipuke in self.lipukkeet:
+            mjono += f"Lipuke # {n}\n"
+            mjono += str(lipuke) + "\n"
+            n += 1
+        
+        return mjono.strip()
+    
+if __name__ == "__main__":
+    lipukelista = Lipukkeet()
+    ehdokas1 = Ehdokas(1)
+    ehdokas2 = Ehdokas(2)
+    lipukelista.lisaa_lipuke(Lipuke([ehdokas1, ehdokas2]))
+    lipukelista.lisaa_lipuke(Lipuke([ehdokas2, ehdokas1]))
+    print(lipukelista)
