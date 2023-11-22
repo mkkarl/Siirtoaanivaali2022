e9a03dad370c542dac90151886f00e868b490954
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2023-11-22 11:57:25 +0200

    Add Lipukerivi __str__

--- siirtoaanivaali2022/Lipukerivi.py
+++ siirtoaanivaali2022/Lipukerivi.py
@@ -1,4 +1,4 @@
-import siirtoaanivaali2022.Ehdokas as Ehdokas
+from Ehdokas import Ehdokas
 import Lipukerivi
 
 class Lipukerivi:
@@ -7,3 +7,13 @@ class Lipukerivi:
         self.ehdokas = ehdokas
         self.edellinen_rivi = edellinen_rivi
         self.aanimaara = aanimaara
+
+    def __str__(self) -> str:
+        return f"{str(self.ehdokas)}, äänimäärä: {self.aanimaara}"
+    
+
+if __name__ == "__main__":
+    rivi1 = Lipukerivi(Ehdokas(1),None, 1)
+    print(rivi1)
+    rivi2 = Lipukerivi(Ehdokas(2),rivi1, 1)
+    print(rivi2)
