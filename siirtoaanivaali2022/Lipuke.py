5d06a9c42db18a92a57b6e395242761b547e54c1
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2023-11-22 12:08:41 +0200

    Add Lipuke __str__

--- siirtoaanivaali2022/Lipuke.py
+++ siirtoaanivaali2022/Lipuke.py
@@ -1,14 +1,25 @@
-import siirtoaanivaali2022.Ehdokas as Ehdokas
-import siirtoaanivaali2022.Lipukerivi as Lipukerivi
+from Ehdokas import Ehdokas
+from Lipukerivi import Lipukerivi
 
 class Lipuke:
 
     def __init__(self, aanet :list):
         # ehdokas pitää saada haettua ehdokaslistasta
-        self.rivit = Lipukerivi(aanet[0], None, 1)
+        self.rivit = []
+        self.rivit.append(Lipukerivi(aanet[0], None, 1))
 
         for n in range(1,len(aanet)):
             self.rivit.append(Lipukerivi(aanet[n], self.rivit[n - 1], 0))
-        
-        
-        
+
+    def __str__(self) -> str:
+        mjono = ""
+        for rivi in self.rivit:
+            mjono += str(rivi) + "\n"
+
+        return mjono.strip()
+    
+
+if __name__ == "__main__":
+    ehdokaslista = [Ehdokas(1), Ehdokas(2)]
+    lipuke = Lipuke(ehdokaslista)
+    print(lipuke)
