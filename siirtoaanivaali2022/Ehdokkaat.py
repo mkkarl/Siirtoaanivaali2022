f47ce5bc1295a0578f3504d968018390128faf91
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2023-11-22 11:47:16 +0200

    Add Ehdokkaat __str__

--- siirtoaanivaali2022/Ehdokkaat.py
+++ siirtoaanivaali2022/Ehdokkaat.py
@@ -9,3 +9,14 @@ class Ehdokkaat:
         for n in range(ehdokkaat_lkm):
             self.ehdokkaat[n+1] = Ehdokas(n+1)
     
+    def __str__(self) -> str:
+        mjono = ""
+        for ehdokas in self.ehdokkaat:
+            mjono += str(self.ehdokkaat[ehdokas]) + "\n"
+        return mjono.strip()
+    
+
+if __name__ == "__main__":
+    ehdokaslista = Ehdokkaat()
+    ehdokaslista.lisaa_ehdokkaat(2)
+    print(ehdokaslista)
